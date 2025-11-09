b50c8584f577547f54ef76e96d09bf01db58cccf
Chr-1824 <27128148@sun.ac.za>
2025-11-09 20:39:12 +0000

    Kom

--- my-admin/src/admin/index.jsx
+++ my-admin/src/admin/index.jsx
@@ -1,15 +1,16 @@
-import { Admin, CustomRoutes } from "react-admin";
+import { Admin, CustomRoutes,ListGuesser, EditGuesser, ShowGuesser } from "react-admin";
 import { Route } from "react-router-dom";
 import { createTrailbaseProvider } from "/workspaces/Last/my-admin/ra-trailbase (1).js";
-import { CreateUser, LoginPage } from "./create_user";
+import { CreateUser, LoginPage } from "/workspaces/Last/my-admin/src/admin/create_user.jsx";
 
-const TRAILBASE_URL = "http://localhost:4000";
+const TRAILBASE_URL = "https://glorious-funicular-9676vwxg74v3pg57-4000.app.github.dev/";
 const { dataProvider, authProvider } = await createTrailbaseProvider(
   TRAILBASE_URL,
 );
 
 export const App = () => (
     <Admin dataProvider={dataProvider}>
+    <Resource name = "Account" List = {ListGuesser} />
 
     </Admin>
 );
