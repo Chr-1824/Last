26b15ed206e8afeba5b6fb79938b8fca502c1e73
Chr-1824 <27128148@sun.ac.za>
2025-11-09 20:57:28 +0000

    edits

--- my-admin/src/admin/index.jsx
+++ my-admin/src/admin/index.jsx
@@ -3,17 +3,24 @@ import { Route } from "react-router-dom";
 import { createTrailbaseProvider } from "/workspaces/Last/my-admin/ra-trailbase (1).js";
 import { CreateUser, LoginPage } from "/workspaces/Last/my-admin/src/admin/create_user.jsx";
 
-const TRAILBASE_URL = "https://glorious-funicular-9676vwxg74v3pg57-4000.app.github.dev/";
+const TRAILBASE_URL = "https://glorious-funicular-9676vwxg74v3pg57-4000.app.github.dev";
 const { dataProvider, authProvider } = await createTrailbaseProvider(
   TRAILBASE_URL,
 );
 
-export const App = () => (
-    <Admin dataProvider={dataProvider}>
-    <Resource name = "Account" List = {ListGuesser} />
-
+const App = () => (
+    <Admin
+      loginPage={LoginPage}
+      dataProvider={dataProvider}
+      authProvider={authProvider}
+    >
+      <CustomRoutes noLayout>
+        <Route path="/create-user" element={<CreateUser />} />
+      </CustomRoutes>
+      <Resource name = "Account" List = {ListGuesser} />
     </Admin>
-);
+  );
+
 
 
 
